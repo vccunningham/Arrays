cf55da6946086141f6b41bc1f20f3a8f6e63642a
max-student <max-student@maxtrain.com>
2020-02-11 12:24:09 -0500

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Arrays {
+    class Program {
+        static void Main(string[] args) {
+
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+            var total = 0;
+            foreach (var number in numbers) {
+                if ((number % 3 == 0) || (number % 5 == 0)) {
+                    continue; // skip it }
+                    }
+                total += number * 3;
+                }
+
+
+            Console.WriteLine($"Total is {total}");
+
+
+            //var randomNumbers = new int[5];
+            //randomNumbers[0] = 123;
+            //randomNumbers[1] = 456;
+            //randomNumbers[2] = 789;
+            //randomNumbers[3] = 1;
+            //randomNumbers[4] = 0;
+
+            //var sum = randomNumbers[0] + randomNumbers[1] + randomNumbers[2];
+
+
+
+            }
+        }
+    }
